c993bdd7566c663e6e6dfb176408bafd7a584342
Bruno Rechiedei <brechied@c3r4s3.42warsaw.pl>
2026-08-18 19:35:38 +0200

    struct

--- push_swap.h
+++ push_swap.h
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brechied <brechied@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/18 18:12:01 by brechied          #+#    #+#             */
+/*   Updated: 2026/08/18 18:12:05 by brechied         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -8,12 +19,28 @@
 
 typedef struct s_stack
 {
-    int             value;
-    struct s_stack  *next;
+	int				value;
+	struct s_stack	*next;
 	struct s_stack	*prev;
-} t_stack;
+}				t_stack;
 
-double compute_disorder(t_stack *a);
+typedef struct s_stacks
+{
+	struct t_stack	*a;
+	struct t_stack	*b;
+	int				c_sa;
+	int				c_sb;
+	int				c_ss;
+	int				c_pa;
+	int				c_pb;
+	int				c_ra;
+	int				c_rb;
+	int				c_rr;
+	int				c_rra;
+	int				c_rrb;
+	int				c_rrr;
+}			t_stacks;
+
+double	compute_disorder(t_stack *a);
 
 #endif
-
