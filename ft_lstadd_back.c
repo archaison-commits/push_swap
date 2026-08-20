e7023158a5d0559ce6292e979d6084266618eeef
archaison-commits <archaison@gmail.com>
2026-08-20 04:16:25 +0200

    Remove commented-out main function from ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -26,25 +26,3 @@ void    ft_lstadd_back(t_stack **lst, t_stack *new)
 	lst1 = ft_lstlast(*lst);
 	lst1->next = new;
 }
-/*
-int	main(void)
-{
-	t_list	*head;
-	t_list	*node1;
-	t_list	*node2;
-	t_list	*node3;
-
-	head = NULL;
-	node1 = ft_lstnew(ft_strdup("HELP1"));
-	node2 = ft_lstnew(ft_strdup("HELP2"));
-	node3 = ft_lstnew(ft_strdup("HELP3"));
-	ft_lstadd_back(&head, node2);
-	ft_lstadd_back(&head, node3);
-	ft_lstadd_back(&head, node1);
-	while (head)
-	{
-		printf("content: %s\n", (char *)head->content);
-		head = head->next;
-	}
-	return (0);
-}*/
