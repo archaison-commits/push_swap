eff1b2a6d6152889a9291f15668d355979aa8c19
archaison-commits <archaison@gmail.com>
2026-08-20 04:16:45 +0200

    Remove commented-out code in ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -20,24 +20,3 @@ t_stack *ft_lstlast(t_stack *lst)
 		lst = lst->next;
 	return (lst);
 }
-/*
-int	main(void)
-{
-	t_list	*head;
-	t_list	*node2;
-	t_list	*node3;
-	t_list	*node4;
-
-	head = NULL;
-	node2 = ft_lstnew(ft_strdup("HELP1"));
-	node3 = ft_lstnew(ft_strdup("HELP2"));
-	node4 = ft_lstnew(ft_strdup("HELP3"));
-	node2->next = node3;
-	node3->next = node4;
-	node4->next = NULL;
-	ft_lstadd_front(&head, node2);
-	ft_lstadd_front(&head, node3);
-	ft_lstadd_front(&head, node4);
-	printf("content: %s\n", (char *)ft_lstlast(head)->content);
-	return (0);
-}*/
